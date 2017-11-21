a8ffc0abf68526f20124818a474e7bdfac67215e
Robin Håkansson <robinhokansson@gmail.com>
2017-11-21 18:39:47 +0100

    First website upload

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$("a").click(function (e) {
+    var btnId = "section"+e.currentTarget.id;
+
+    $('html, body').animate({
+        scrollTop: $("#" + btnId).offset().top
+    }, 1000);
+});
+var  mn = $(".logo-scroll");
+    mns = "main-nav-scrolled";
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() >550 ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
+
+var  mg = $(".para1");
+    mgn = "menu-gone";
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() >1700 ) {
+    mg.addClass(mgn);
+  } else {
+    mg.removeClass(mgn);
+  }
+});
+
+var  mc = $(".contacts");
+    mcn = "menu-gone";
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() >550 ) {
+    mc.addClass(mcn);
+  } else {
+    mc.removeClass(mcn);
+  }
+});
